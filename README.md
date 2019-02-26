4ee4b6a7a324b5512abd2831216a8b7aef6431fa
lucasgoncdasilva <lucasgoncdasilva@gmail.com>
2019-02-26 19:46:25 -0300

    Coleta dos dados do cadastro de turmas

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # cagr
-trabalho de universidade
+
+Repositório de alunos da UFSC, na matéria de Teoria da Decisão, dada em fevereiro de 2019.
+
+### Alunos:
+André Gustavo Lucas
+Íris Tramontim
+Lucas Gonçalves da Silva
--- main.py
+++ main.py
@@ -8,6 +8,7 @@ import logging
 import pandas as pd
 import dotenv
 import os
+import time
 
 dotenv.load_dotenv()
 
@@ -17,6 +18,7 @@ senha = os.getenv('SENHA_CAGR')
 Chrome = webdriver.Chrome('drivers/chromedriver')
 url = 'https://cagr.sistemas.ufsc.br/modules/aluno/'
 Chrome.get(url)
+
 campo_username = Chrome.find_element_by_css_selector('#username')
 campo_username.send_keys(usuario)
 
@@ -26,6 +28,56 @@ campo_password.send_keys(senha)
 botao_enter = Chrome.find_element_by_css_selector('#fm1 > div > input')
 botao_enter.click()
 
+# Caso usuário precise preencher pesquisa do CAGR, clicar para voltar para o CAGR
+try:
+    botao_voltar_cagr = Chrome.find_element_by_css_selector('#j_id20 > input[type="submit"]:nth-child(2)')
+    botao_voltar_cagr.click()
+except:
+    pass
+
 botao_cadastro_turma = Chrome.find_element_by_css_selector('#aluno9-10-10')
 botao_cadastro_turma.click()
 
+
+def get_data(driver):
+    """Carrega os dados da tabela de cadastro de turmas."""
+    
+    tabela = driver.find_element_by_xpath('//*[@id="formBusca:dataTable"]')
+
+    dfs = pd.read_html(tabela.get_attribute('outerHTML'), index_col=0)
+
+    return dfs[0]
+
+next_button = Chrome.find_element_by_xpath('//*[@id="formBusca:dataScroller1_table"]/tbody/tr/td[15]')
+
+data = pd.DataFrame()
+
+for i in range(108):
+
+    new_data = get_data(Chrome)
+
+    data = data.append(new_data, sort=False)
+
+    try:
+        next_button = Chrome.find_element_by_xpath('//*[@id="formBusca:dataScroller1_table"]/tbody/tr/td[15]')
+        next_button.click()
+        time.sleep(3)
+
+    except:
+        print('Saindo do Loop....')
+        break
+    
+
+print('Antes de dropar os duplicados, forma do data frame era:')
+print(data.shape)
+data = data.drop_duplicates()
+print('Depois de dropar:')
+print(data.shape)
+
+print(data)
+print(data.size)
+
+print(data.tail())
+
+
+data.to_csv('Dados CAGR.csv', sep=';')
